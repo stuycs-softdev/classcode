8e4414e73654abc1ae96096713d2214bd42d6f1e
Mike Zamansky <zamansky@gmail.com>
2015-09-25 11:42:30 -0400

    Z

--- 5/flask-intro/app.py
+++ 5/flask-intro/app.py
@@ -20,16 +20,41 @@ def oldaboutpage():
 """
     return page
 
+@app.route("/person")
+@app.route("/person/")
+@app.route("/person/<lastname>")
+@app.route("/person/<lastname>/<firstname>")
+def person(lastname="",firstname=""):
+    if lastname!="zamansky":
+        error="WRONG NAME, TRY AGAIN"
+    else:
+        error="YOU WIN"
+    d = {'last':lastname,
+         'first':firstname,
+         'title':"Fool Pitier",
+         'error':error}
+
+    l = [10,20,31,45,1234,'hello','item',22]
+    
+    return render_template("person.html",d=d,list=l)
+
 @app.route("/lucky")
 def lucky_number():
     import random
     r = random.randrange(1,100)
-    return "<h1>Lucky Number: %d</h1>" % (r)
+    # return "<h1>Lucky Number: %d</h1>" % (r)
+    return render_template("lucky.html",number=r)
     
 @app.route("/home")
 @app.route("/")
 def kerfuffle():
-    return "<h1>Home Page</h1><h2>Hello World</h2>"
+    page="""
+    <h1>Home Page</h1><h2>Hello World</h2>
+    <hr>
+    <button><a href="/about">about</a></button>
+"""
+    return page
+
 
 if __name__ == "__main__":
     app.debug = True
