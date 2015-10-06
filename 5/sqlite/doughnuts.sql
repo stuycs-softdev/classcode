6502e6080bf72b41a6357779a5b9e7fc2dced568
Mike Zamansky <zamansky@gmail.com>
2015-10-06 12:37:49 -0400

    Z

---
+++ 5/sqlite/doughnuts.sql
@@ -0,0 +1,4 @@
+create table doughnuts (type text, price real, qty integer);
+insert into doughnuts values ("jelly", 2.50, 10);
+insert into doughnuts values ("glazed", 3.00 ,20);
+insert into doughnuts values ("chocolate", 2.00, 5);
---
+++ 6/sqlite/doughnuts.sql
@@ -0,0 +1,6 @@
+create table doughnuts (name text, price real, qty integer);
+insert into doughnuts values ("jelly",2.50,10);
+insert into doughnuts values ("glazed",2.00,20);
+insert into doughnuts values ("chocolate",3.00,5);
+insert into doughnuts values ("coconut",3.50,25);
+insert into doughnuts values ("passion fruit",1.50,6);
