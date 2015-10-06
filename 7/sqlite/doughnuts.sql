52729d3956dd49304fe9b754e921c2c2bf4bf9d4
Mike Zamansky <zamansky@gmail.com>
2015-10-06 13:19:27 -0400

    Z

---
+++ 7/sqlite/doughnuts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE doughnuts (name TEXT, price REAL, qty INTEGER);
+INSERT INTO doughnuts VALUES ("jelly",2.50,10);
+INSERT INTO doughnuts VALUES ("glazed",2.00,15);
+INSERT INTO doughnuts VALUES ("chocolate",3.00,5);
+INSERT INTO doughnuts VALUES ("coconut",3.50,20);
+INSERT INTO doughnuts VALUES ("boston cream",2.25,12);
