9b766bfd6de94819bc3c4df86001d50d6847b721
Mike Zamansky <zamansky@gmail.com>
2015-10-07 12:43:37 -0400

    Z

---
+++ 7/sqlite/classes.sql
@@ -0,0 +1,13 @@
+create table classes (name,grade,id);
+
+insert into classes values ("APCS",80,1);
+insert into classes values ("APCS",94,4);
+insert into classes values ("APCS",88,3);
+insert into classes values ("APCS",85,7);
+insert into classes values ("APCS",100,5);
+insert into classes values ("CALC",96,4);
+insert into classes values ("ENG",55,6);
+insert into classes values ("DRAFTING",65,1);
+insert into classes values ("DRAFTING",65,7);
+insert into classes values ("DRAFTING",65,6);
+insert into classes values ("DRAFTING",65,3);
