e23b1f73b9b4fd99a5d6932b01fb1a75cb283119
Mike Zamansky <zamansky@gmail.com>
2015-10-07 12:33:27 -0400

    Z

---
+++ 6/sqlite/people.sql
@@ -0,0 +1,10 @@
+CREATE TABLE people (name text, age integer, id integer);
+
+insert into people values ("homer",40,1);
+insert into people values ("marge",40,2);
+insert into people values ("bart",10,3);
+insert into people values ("lisa",8,4);
+insert into people values ("maggie",4,5);
+insert into people values ("ralph",8,6);
+insert into people values ("nelson",16,7);
+insert into people values ("kierney",18,8);
