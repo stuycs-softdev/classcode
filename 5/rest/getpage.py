c66384bd96beae775603e85d6449784e58ad3536
Mike Zamansky <zamansky@gmail.com>
2015-11-04 11:46:03 -0500

    Z

--- 5/rest/getpage.py
+++ 5/rest/getpage.py
@@ -1,4 +1,5 @@
 import urllib2
+import json
 
 #url="http://www.weather.com/weather/today/l/10001:4:US"
 
@@ -9,4 +10,10 @@ url = "http://api.openweathermap.org/data/2.5/weather?q=NYC&units=Imperial&appid
 print url
 request = urllib2.urlopen(url)
 result = request.read()
-print result
+r = json.loads(result)
+print r
+print r.keys()
+print r['main']['temp']
+#d={'name':'thomas','age':30}
+#print json.dumps(d)
+
