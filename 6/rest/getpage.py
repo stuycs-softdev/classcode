c08a25ecd6a1480a14f60fc7aeda4426685f715a
Mike Zamansky <zamansky@gmail.com>
2015-11-05 11:36:30 -0500

    Z

--- 6/rest/getpage.py
+++ 6/rest/getpage.py
@@ -1,10 +1,15 @@
 import urllib2
+import json
 
 #url = "https://www.google.com/#safe=off&q=iguana"
-url='http://api.openweathermap.org/data/2.5/weather?q="nome"&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial'
+url='http://api.openweathermap.org/data/2.5/weather?q="nyc"&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial'
 
 
 request = urllib2.urlopen(url)
 result = request.read()
-
-print result
+r = json.loads(result)
+print r
+print
+print r.keys()
+print r['main']
+print r['main']['temp']
--- 7/rest/getpage.py
+++ 7/rest/getpage.py
@@ -1,4 +1,6 @@
 import urllib2
+import json
+
 
 #url="http://www.nytimes.com"
 
@@ -7,5 +9,10 @@ import urllib2
 url="http://api.openweathermap.org/data/2.5/weather?q=NYC&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial"
 request = urllib2.urlopen(url)
 result = request.read()
-print result
+r = json.loads(result)
+print r
+print
+print r.keys()
+print r['main']
+print r['main']['temp']
 
