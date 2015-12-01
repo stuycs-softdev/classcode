d186b78682df06322635928f489db9a5933d789b
Mike Zamansky <zamansky@gmail.com>
2015-12-01 13:18:32 -0500

    z0

--- 7/js/intro.js
+++ 7/js/intro.js
@@ -11,3 +11,48 @@ var d = {
 		k2 : 123,
 		123 : [1,2,3,4,5]
 };
+
+function f(n){
+		var x = 30;
+		console.log("In f: "+x+n);
+}
+
+var fact = function fact (n){
+		var prod=1;
+		for ( ; n>1; n--){
+				prod = prod * n;
+		}
+		return prod;
+};
+
+var addItem = function addItem(s){
+		var l = document.getElementById("thelist");
+		var n = document.createElement("li");
+		n.innerHTML=s;
+		l.appendChild(n);
+};
+
+
+var removeItem = function removeItem(n){
+		var items = document.getElementsByTagName("li");
+		items[n].remove();
+};
+
+
+var color = function(){
+		var items = document.getElementsByTagName("li");
+		for (var i = 0; i < items.length; i++){
+				items[i].classList.add("red");
+		}
+};
+
+var stripe = function(){
+		var items = document.getElementsByTagName("li");
+		for (var i = 0; i < items.length; i++){
+				if (i%2==0)
+						items[i].classList.add("red");
+				else
+						items[i].classList.add("green");
+		}
+};;
+
